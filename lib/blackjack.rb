d09494f04aaa6d01a82f9ed3d12f7f99fef59564
Umair Qaisar <qaisaru@icloud.com>
2018-09-13 11:06:07 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,66 +1,51 @@
-def deal_card
-  # code #deal_card here
-    rand(1..11)
-end
-
-def display_card_total(card_total)
-  # code #display_card_total here
-  puts "Your cards add up to #{card_total}"
-  card_total
-end
-
 def welcome
-  # code #welcome here
-  puts "Welcome to the Blackjack Table"
+    puts "Welcome to the Blackjack Table"
+end
+
+def deal_card
+    return rand(1..11)
+end
+
+def display_card_total(n)
+  puts "Your cards add up to #{n}"
 end
 
 def prompt_user
-  # code #prompt_user here
   puts "Type 'h' to hit or 's' to stay"
 end
 
-def initial_round
-  # code #initial_round here
-  sum = rand(1..11) + rand(1..11)
-  display_card_total(sum)
-end
-
-def end_game(card_total)
-  # code #end_game here
-    puts "Sorry, you hit #{card_total}. Thanks for playing!"
-end
-
 def get_user_input
-  # code #get_user_input here
-  gets.strip.chomp
+  method= gets.chomp
 end
 
-def hit? (card_total)
-  # code hit? here
-  prompt_user
-  if get_user_input == 'h'
-    card_total += deal_card
+def end_game(n)
+  if n>21
+    puts "Sorry, you hit #{n}. Thanks for playing!"
   end
-    card_total
 end
 
-def invalid_command
-  # code invalid_command here
-  puts "invalid"
+def initial_round
+  sum= deal_card + deal_card
+  display_card_total(sum)
+  return sum
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
+def hit?(current_total)
+  prompt_user
+  input= get_user_input
+  if input == "h"
+    current_total+= deal_card
+  elsif input == "s"
+    return current_total
+  end
+end
 
 def runner
-  # code runner here
   welcome
-  card_total = initial_round
-  while card_total < 21
-    card_total = hit?(card_total)
-    card_total = display_card_total(card_total)
+  hand=initial_round
+  until hand>21 do
+    hand=hit?(hand)
+    display_card_total(hand)
   end
-  end_game(card_total)
-
+  end_game(hand)
 end
