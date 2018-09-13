beb085966a92580da023e7b59a6bbfccebb5bf2b
Umair Qaisar <qaisaru@icloud.com>
2018-09-13 11:04:27 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -49,15 +49,3 @@ def invalid_command
   # code invalid_command here
   puts "invalid"
 end
-
-def runner
-  # code runner here
-  welcome
-  card_total = initial_round
-  while card_total < 21
-    card_total = hit?(card_total)
-    card_total = display_card_total(card_total)
-  end
-  end_game(card_total)
-
-end
