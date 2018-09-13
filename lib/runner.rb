595348b1f6e1ec7d9cab89af3b7f590af116f3ae
Umair Qaisar <qaisaru@icloud.com>
2018-09-13 11:04:04 +0000

    automatically backed up by learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,15 @@
 require_relative "blackjack.rb"
 
 runner
+
+def runner
+  # code runner here
+  welcome
+  card_total = initial_round
+  while card_total < 21
+    card_total = hit?(card_total)
+    card_total = display_card_total(card_total)
+  end
+  end_game(card_total)
+
+end
